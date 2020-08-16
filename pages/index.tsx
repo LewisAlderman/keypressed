0511cd07c826413138e05c164b1dcfde1a4e53d4
Lewis Alderman <lewisalderman95@gmail.com>
2020-08-16 17:05:22 +0100

    Added next-pwa and manifest/favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,36 @@ export default function Home() {
     <div>
       <Head>
         <title>WhichKey</title>
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="Which key pressed? Keyboard event info visualizer"
+        />
+        <meta
+          name="keywords"
+          content="Keyboard key press event javascript info"
+        />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="images/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="images/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="images/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
       </Head>
 
       <h1>Press a key</h1>
