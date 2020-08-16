61b1494064d5ea6e5d177464e55edae36b5270fe
Lewis Alderman <lewisalderman95@gmail.com>
2020-08-16 16:01:23 +0100

    Setup eslint/prettier (incl. for TS)

---
+++ src/settings.ts
@@ -0,0 +1,29 @@
+export const EVENT_KEYS = [
+  'altKey',
+  'charCode',
+  'code',
+  'ctrlKey',
+  'detail',
+  'key',
+  'keyCode',
+  'location',
+  'metaKey',
+  'returnValue',
+  'shiftKey',
+  'which',
+];
+
+export interface IKeyEventProperties {
+  altKey?: boolean;
+  charCode?: number;
+  code?: string;
+  ctrlKey?: boolean;
+  detail?: number;
+  key?: string;
+  keyCode?: number;
+  location?: number;
+  metaKey?: boolean;
+  returnValue?: true;
+  shiftKey?: boolean;
+  which?: number;
+}
